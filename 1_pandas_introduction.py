24ea6e6f4d8f065b1757d31e050c9ec47236e8ec
vivy-c <cahyanivivy@gmail.com>
2024-04-07 00:36:20 +0700

    initial commit

---
+++ 1_pandas_introduction.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+reviews = pd.read_csv("../input/wine-reviews/winemag-data-130k-v2.csv", index_col=0)
+pd.set_option("display.max_rows", 5)
+
+from learntools.core import binder; binder.bind(globals())
+from learntools.pandas.indexing_selecting_and_assigning import *
+print("Setup complete.")
